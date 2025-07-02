d6b4fead6ab99101649cf017f33c960acad571fd
Inbezemer <94757357+Inbezemer@users.noreply.github.com>
2025-07-03 00:13:26 +0200

    Revised Design
    New design, more professional

--- scripts.js
+++ scripts.js
@@ -6,7 +6,7 @@ if (blob){
 	document.onmousemove = (e) => {
 		let x = String(e.clientX) + 'px';
 		let y = String(e.clientY) + 'px';
-		blob.animate({left: x,top: y}, { duration: 3000, fill: "forwards" });
+		blob.animate({left: x,top: y}, { duration: 300, fill: "forwards" });
 	}
 } 
 
@@ -20,3 +20,30 @@ document.getElementById("project-gallery").onmousemove = e => {
     card.style.setProperty("--mouse-y", `${y}px`);
   };
 }
+
+/* -- Parallax -- */
+const gallery = document.getElementById("project-gallery");
+gallery.addEventListener('scroll', () => {
+	for (const img of document.getElementsByClassName('card-content')) {
+		const newpos = `${(gallery.scrollLeft / gallery.scrollWidth) * 100}% 30%`;
+		img.style.setProperty("background-position", newpos);
+	}
+});
+
+/* -- Transition -- */
+const observer = new IntersectionObserver((entries) => {
+	entries.forEach((entry) => {
+		console.log(entry);
+		if (entry.isIntersecting) {
+			if (entry.target.classList.contains('hidden')) {
+				entry.target.classList.add('show');
+			} else {
+				entry.target.classList.add('show-delay');
+			}
+		}
+	});
+});
+
+const hiddenElements = document.querySelectorAll('.hidden, .hidden-delay');
+hiddenElements.forEach((el) => observer.observe(el));
+											
